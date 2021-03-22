2356adc7f0a19c4671c5670688f9588768301964
Max Lima <max.lima2@gmail.com>
2021-03-21 22:40:37 -0300

    change api base url to production

--- lib/app/shared/utils/constants.dart
+++ lib/app/shared/utils/constants.dart
@@ -1,2 +1,2 @@
-// 'https://manga-unifier.herokuapp.com/api/v1';
-const String BASE_URL = 'http://192.168.100.244:8000/api/v1';
+const String BASE_URL = 'https://manga-unifier.herokuapp.com/api/v1';
+// const String BASE_URL = 'http://192.168.100.244:8000/api/v1';
