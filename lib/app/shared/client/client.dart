43404c29c57c2f13c22bed6aba2abaf4b6f0bb48
Max Lima <max.lima2@gmail.com>
2021-03-21 22:40:53 -0300

    change log interceptor to dio implementation

--- lib/app/shared/client/client.dart
+++ lib/app/shared/client/client.dart
@@ -1,5 +1,5 @@
+import 'package:dio/dio.dart';
 import 'package:dio/native_imp.dart';
-import 'package:unifier_mobile/app/shared/client/log_interceptor.dart' as l;
 import 'package:unifier_mobile/app/shared/utils/constants.dart';
 
 import 'header_interceptor.dart';
@@ -7,4 +7,4 @@ import 'header_interceptor.dart';
 DioForNative client = DioForNative()
   ..options.baseUrl = BASE_URL
   ..interceptors.add(HeaderInterceptor())
-  ..interceptors.add(l.LogInterceptor());
+  ..interceptors.add(LogInterceptor());
