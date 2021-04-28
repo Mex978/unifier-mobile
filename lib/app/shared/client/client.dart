c581ed2d6345bf4e27afec772f32d10cae066cb2
Max Lima <max.lima2@gmail.com>
2021-04-28 16:15:48 -0300

    remove logs from dio

--- lib/app/shared/client/client.dart
+++ lib/app/shared/client/client.dart
@@ -2,9 +2,7 @@ import 'package:dio/native_imp.dart';
 import 'package:unifier_mobile/app/shared/utils/constants.dart';
 
 import 'header_interceptor.dart';
-import 'log_interceptor.dart';
 
 DioForNative client = DioForNative()
   ..options.baseUrl = BASE_URL
-  ..interceptors.add(HeaderInterceptor())
-  ..interceptors.add(LogInterceptor());
+  ..interceptors.add(HeaderInterceptor());
