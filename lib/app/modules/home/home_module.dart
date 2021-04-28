a88955d38201f29aa836df01b672ddf96d8abd78
Max Lima <max.lima2@gmail.com>
2021-04-28 17:27:42 -0300

    returns home screen navigation to previous approach

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,10 +1,7 @@
-import 'package:unifier_mobile/app/modules/home/widgets/home_mangas_view/home_mangas_view_widget.dart';
-import 'package:unifier_mobile/app/modules/home/widgets/home_novels_view/home_novels_view_widget.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 import './repositories/home_repository.dart';
 import 'home_controller.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-
 import 'home_page.dart';
 
 class HomeModule extends Module {
@@ -19,18 +16,6 @@ class HomeModule extends Module {
     ChildRoute(
       Modular.initialRoute,
       child: (_, args) => HomePage(),
-      children: [
-        ChildRoute(
-          '/mangas',
-          child: (_, __) => HomeMangasViewWidget(),
-          transition: TransitionType.fadeIn,
-        ),
-        ChildRoute(
-          '/novels',
-          child: (_, __) => HomeNovelsViewWidget(),
-          transition: TransitionType.fadeIn,
-        ),
-      ],
     ),
   ];
 }
